8d939755326fd84d7ecc627e508dd338c1af5a15
kawakicchi <kawakicchi@gmail.com>
2015-01-24 09:02:32 +0900

    リクエストメソッド追加

--- src/main/java/org/azkfw/jersey/test/AbstractJerseyTestCase.java
+++ src/main/java/org/azkfw/jersey/test/AbstractJerseyTestCase.java
@@ -1,5 +1,6 @@
 package org.azkfw.jersey.test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.HashSet;
@@ -8,15 +9,22 @@ import java.util.Map;
 import java.util.Set;
 
 import javax.ws.rs.ProcessingException;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import junit.framework.TestCase;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.CustomServletContainer;
 import org.glassfish.jersey.test.DeploymentContext;
@@ -25,13 +33,19 @@ import org.glassfish.jersey.test.TestProperties;
 import org.glassfish.jersey.test.spi.TestContainer;
 import org.glassfish.jersey.test.spi.TestContainerException;
 import org.glassfish.jersey.test.spi.TestContainerFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public abstract class AbstractJerseyTestCase extends TestCase {
 
 	private JerseyTest jerseyTest;
 
 	protected abstract Map<String, String> getInitParams();
 
+	@Before
 	@Override
 	public void setUp() {
 		//super.setUp();
@@ -46,6 +60,7 @@ public abstract class AbstractJerseyTestCase extends TestCase {
 		}
 	}
 
+	@After
 	@Override
 	public void tearDown() {
 		try {
@@ -115,6 +130,7 @@ public abstract class AbstractJerseyTestCase extends TestCase {
 							}
 						}
 
+						@SuppressWarnings("deprecation")
 						@Override
 						public void stop() {
 							this.server.stop();
@@ -125,5 +141,116 @@ public abstract class AbstractJerseyTestCase extends TestCase {
 
 			};
 		}
+	}
+
+	/**
+	 * GETメドッド
+	 * 
+	 * @param path パス
+	 * @return レスポンス情報
+	 */
+	protected final Response get(final String path) {
+		return get(path, null);
+	}
+
+	/**
+	 * GETメソッド
+	 * 
+	 * @param path パス
+	 * @param params パラメータ
+	 * @return レスポンス情報
+	 */
+	protected final Response get(final String path, final Map<String, Object> params) {
+		WebTarget target = target(path);
+		if (null != params) {
+			for (String key : params.keySet()) {
+				Object value = params.get(key);
+				target = target.queryParam(key, value);
+			}
+		}
+		Response response = target.request().get();
+		return response;
+	}
+
+	/**
+	 * POSTメソッド
+	 * 
+	 * @param path パス
+	 * @param entity パラメータ
+	 * @return レスポンス情報
+	 */
+	protected final Response post(final String path, final Entity<?> entity) {
+		Response response = target(path).request().post(entity);
+		return response;
+	}
+
+	/**
+	 * POSTメソッド(Form形式)
+	 * 
+	 * @param path パス
+	 * @param params パラメータ
+	 * @return レスポンス情報
+	 */
+	protected final Response postForm(final String path, final Map<String, String> params) {
+		Form form = new Form();
+		for (String key : params.keySet()) {
+			form.param(key, params.get(key));
+		}
+		return postForm(path, form);
+	}
+
+	/**
+	 * POSTメソッド(Form形式)
+	 * 
+	 * @param path パス
+	 * @param entity パラメータ
+	 * @return レスポンス情報
+	 */
+	protected final Response postForm(final String path, final Form form) {
+		Entity<Form> entity = Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE);
+		return post(path, entity);
+	}
+
+	/**
+	 * POSTメソッド(JSON形式)
+	 * 
+	 * @param path パス
+	 * @param object パラメータ
+	 * @return レスポンス情報
+	 */
+	protected final Response postJson(final String path, final Object object) {
+		Entity<Object> entity = Entity.entity(object, MediaType.APPLICATION_JSON_TYPE);
+		return post(path, entity);
+	}
+
+	/**
+	 * POSTメソッド(MultiPart形式)
+	 * 
+	 * @param path パス
+	 * @param params パラメータ
+	 * @return 結果
+	 * @throws IOException
+	 */
+	protected final Response postMultiPart(final String path, final Map<String, Object> params) throws IOException {
+		FormDataMultiPart multiPart = new FormDataMultiPart();
+		if (null != params) {
+			for (String key : params.keySet()) {
+				Object obj = params.get(key);
+				if (null == obj) {
+
+				} else if (obj instanceof File) {
+					File file = (File) obj;
+					FileDataBodyPart fileDataBodyPart = new FileDataBodyPart(key, file);
+					multiPart.bodyPart(fileDataBodyPart);
+				} else {
+					String string = obj.toString();
+					FormDataBodyPart formDataBodyPart = new FormDataBodyPart(key, string);
+					multiPart.bodyPart(formDataBodyPart);
+				}
+			}
+		}
+		Entity<FormDataMultiPart> entity = Entity.entity(multiPart, MediaType.MULTIPART_FORM_DATA_TYPE);
+		Response response = target(path).request().header("Content-type", MediaType.MULTIPART_FORM_DATA).post(entity);
+		return response;
 	}
 }
